57fb0ffba6ca06b942c868dafb4cf2a307b9f68a
Anirudh Kumar Subramanyam <ask710@uw.edu>
2017-03-02 22:33:24 -0800

    Add initial files

---
+++ server.R
@@ -0,0 +1,10 @@
+library(leaflet)
+library(shiny)
+library(httr)
+library(jsonlite)
+
+server <- function(input, output){
+  
+}
+
+shinyServer(server)
---
+++ ui.R
@@ -0,0 +1,9 @@
+library(leaflet)
+library(shiny)
+library(httr)
+library(jsonlite)
+ui <- fluidPage(
+  
+)
+
+shinyUI(ui)
