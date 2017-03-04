5341af6a4af9b96a39be2113cd4aa1b975013ff6
Anirudh Kumar Subramanyam <ask710@uw.edu>
2017-03-03 19:29:38 -0800

    Add bounds in progress

--- server.R
+++ server.R
@@ -11,29 +11,46 @@ server <- function(input, output){
   locationData <- reactive({
     query = list(location = input$chosen.location)
     response <-GET ("https://api.yelp.com/v3/businesses/search?term=food&limit=50", query = query, add_headers(Authorization = access.code))
-    print(response)
+    #print(response)
     body <- fromJSON(content(response, "text"))
     data <- body$businesses
     return(data)
   })
   
+  getBounds <- reactive({
+    if(input$chosen.location == "")
+      print(input$map_bounds)
+  })
+  
   #creates map based on data above
   output$map <- renderLeaflet({
-    coordinates <- locationData()$coordinates
+    
+      
+    # print(input$map_bounds)
+    # bounds <- input$map_bounds
+    # m <- leaflet() %>% 
+    #   addTiles() %>% 
+    #   setView(lng = bounds$north, lat = bounds$west, zoom = 7)
+    
+    coordinates <- locationData()$coordinates  
     if(is.null(coordinates)){
       m <- leaflet() %>% 
-            addTiles() %>% 
-            setView(lng = -94, lat = 37.45, zoom = 4)
+        addTiles() %>% 
+        setView(lng = -94, lat = 37.45, zoom = 4)
     }
+    
     else{
+      
       m <- leaflet() %>%
         addTiles() %>%  # Add default OpenStreetMap map tiles
-        addMarkers(lng= coordinates$longitude, lat= coordinates$latitude, popup= paste(data$name, "<br>",
-                                                                                       "Price:", data$price,"<br>",
-                                                                                       "Rating:", data$rating))
+        addMarkers(lng= coordinates$longitude, lat= coordinates$latitude, popup= paste(locationData()$name, "<br>",
+                                                                                       "Price:", locationData()$price,"<br>",
+                                                                                      "Rating:", locationData()$rating))
     }
-    return(m)
+  
+  return(m)
   })
+  
   
 }
 
