aaa7cf11a03a41d3b94fb32b0daf850c64a160cb
Marcus Ta-Gwong Huang <mtghuang@uw.edu>
2017-03-08 17:52:00 -0800

    make the links work

--- server.R
+++ server.R
@@ -17,8 +17,8 @@ states <- geojsonio::geojson_read("json/us-states.json", what = "sp")
 restaurant.ratings <- read.csv("data/yelpRatings.csv", stringsAsFactors = FALSE)
 colnames(restaurant.ratings) <- c("ratings", "county", "NAME")
 restaurant.ratings <- restaurant.ratings %>% 
-                      group_by(NAME) %>% 
-                      summarise(ratings = mean(ratings))
+  group_by(NAME) %>% 
+  summarise(ratings = mean(ratings))
 
 #To prevent unsuccessful merge due to different spelling.
 restaurant.ratings[12, 1] <- "Hawaii"
@@ -31,34 +31,34 @@ pal <- colorBin(palette = "Reds",
 
 #Makes labels for the choropleth map when hovered over each state.
 labels <- sprintf("<strong>%s</strong><br/><strong> Rating: </strong> <em>%g</em> ",
-                as.character(restaurant.data$NAME), round(restaurant.data$ratings, 2)) %>% 
-                lapply(htmltools::HTML)
+                  as.character(restaurant.data$NAME), round(restaurant.data$ratings, 2)) %>% 
+  lapply(htmltools::HTML)
 
 
 
 #makes the choropleth map
- choropleth <- leaflet(restaurant.data) %>%
-              setView(lng = -94, lat = 37.45, zoom = 4) %>% 
-              addProviderTiles(providers$CartoDB.Positron) %>%
-               addLegend(pal = pal, values = ~density, opacity = 0.7, title = "Ratings",
-                           position = "bottomright") %>% 
-               addPolygons(fillColor = ~pal(ratings),
-                          weight = 1, #2
-                          opacity = 1,
-                          color = "white",
-                          dashArray = "1",
-                          fillOpacity = 0.7,
-                          highlight = highlightOptions(
-                                        weight = 3, #5
-                                        color = "#666",
-                                        dashArray = "",
-                                        fillOpacity = 0.7,
-                                        bringToFront = TRUE),
-                          label = labels,
-                          labelOptions = labelOptions(
-                                          style = list("font-weight" = "normal", padding = "3px 8px"),
-                                          textsize = "15px",
-                                          direction = "auto"))   
+choropleth <- leaflet(restaurant.data) %>%
+  setView(lng = -94, lat = 37.45, zoom = 4) %>% 
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addLegend(pal = pal, values = ~density, opacity = 0.7, title = "Ratings",
+            position = "bottomright") %>% 
+  addPolygons(fillColor = ~pal(ratings),
+              weight = 1, #2
+              opacity = 1,
+              color = "white",
+              dashArray = "1",
+              fillOpacity = 0.7,
+              highlight = highlightOptions(
+                weight = 3, #5
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE),
+              label = labels,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto"))   
 
 choropleth 
 
@@ -69,8 +69,8 @@ colnames(static.data) <- data.names
 
 #groups data by state
 state.data <- static.data %>% 
-              group_by(state) %>% 
-              summarise('Average Rating' = round(mean(rating), 2))
+  group_by(state) %>% 
+  summarise('Average Rating' = round(mean(rating), 2))
 
 server <- function(input, output){
   
@@ -90,38 +90,41 @@ server <- function(input, output){
   
   #creates map based on data above. Displays choropleth map if 
   #nothing is entered in the filters.
+  
   output$map <- renderLeaflet({
-    coordinates <- locationData()$coordinates  
-    if(is.null(coordinates)){
-      m <- choropleth
-    }
-     else if(input$click){
-       print(input$map_bounds_click)
-     
+    
+    if(input$chosen.location == ""){
+      if(is.null(input$map_shape_click)){  
+        m <- choropleth
       }
-  
-    else{
-      m <- leaflet() %>%
-      addProviderTiles(providers$CartoDB.Positron) %>% 
-        addMarkers(lng= coordinates$longitude, lat= coordinates$latitude, popup= paste(locationData()$name, "<br>",
-                                                                                       "Price:", locationData()$price,"<br>",
-                                                                                       "Rating:", locationData()$rating))
-    }
-  
-  return(m)
-})
-  
-  #Outputs table with state and rating when choropleth map is displayed,
-  #and outputs table with restaurant and rating when filters are used.
-  output$table <- renderDataTable({
-    if(is.null(locationData())){
-      state.data.names <- c("State", "Average Rating")
-      colnames(state.data) <- state.data.names
-      return(state.data)
+      else{
+        data.bound <- locationDataByBounds()
+        rest.coordinates <- data.bound$coordinates 
+        m <- leaflet() %>%
+          addProviderTiles(providers$CartoDB.Positron) %>% 
+          addMarkers(lng= rest.coordinates$longitude, lat= rest.coordinates$latitude, popup= paste(data.bound$name, "<br>",
+                                                                                                   "Price:", data.bound$price,"<br>",
+                                                                                                   "Rating:", data.bound$rating, "<br>"
+          ))
+      }
+      
+      
+      
     }else{
-    final.frame <- locationData() %>% select(name, rating)
-    return(final.frame)
+      coordinates <- locationData()$coordinates  
+      
+      m <- leaflet() %>%
+        addProviderTiles(providers$CartoDB.Positron) %>% 
+        addMarkers(lng= coordinates$longitude, lat= coordinates$latitude, popup= paste(paste0("<a href='",locationData()$url,"'>",locationData()$name,"</a>"), "<br>",
+                                                                                       "Price:", locationData()$price,"<br>",
+                                                                                       "Rating:", locationData()$rating, "<br>"
+                                                                                  
+                                                                                       #img(src = locationData()$image_url)
+        ))
     }
+    
+    
+    return(m)
   })
   
 }
